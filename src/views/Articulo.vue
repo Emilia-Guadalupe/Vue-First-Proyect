d4a7f38c67f433ae96ce6be6ff0a5904be21530f
Emilia <m.emilia.guadalupe@gmail.com>
2021-02-14 20:52:40 -0300

    Armando proyecto de prueba de Vue

---
+++ src/views/Articulo.vue
@@ -0,0 +1,38 @@
+<template>
+    <Titulo texto="Ruta con parámetros" />
+    <h3> {{articulo.title}} </h3>
+    <p> {{articulo.id}} - {{articulo.body}} </p>
+</template>
+
+<script>
+import Titulo from '../components/Titulo'
+export default {
+    components: {
+        Titulo
+    },
+    data() {
+        return {
+            articulo: {}
+        }
+    },
+    methods: {
+        async consumirArticulo() {
+            try {
+                const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+                const objeto = await data.json()
+                console.log(objeto)
+                this.articulo = objeto
+            } catch (error) {
+                console.log('Hubo un error')
+            }
+        }
+    },
+    created(){
+        this.consumirArticulo()
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Blog.vue
@@ -0,0 +1,42 @@
+<template>
+    <Titulo texto="Mi blog" />
+    <button @click="consumirAPI">Consumir API</button>
+    <div v-for="item in arrayBlog" :key="item.id">
+        <router-link :to="`/blog/${item.id}`">
+            {{item.title}}
+        </router-link>
+    </div>
+</template>
+
+<script>
+import Titulo from '../components/Titulo';
+export default {
+    components: {
+        Titulo
+    }, 
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+    methods: {
+        async consumirAPI() {
+            try {
+                const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+                const array = await data.json()
+                console.log(array);
+                this.arrayBlog = array;
+            } catch (error) {
+                console.log('Hubo un error')
+            }
+        }
+    },
+    created() {
+        this.consumirAPI()  
+    }
+}
+</script>
+
+<style>
+
+</style>
